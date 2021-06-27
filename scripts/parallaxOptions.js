bbcca3cb76028061dacfd5f07bd8463ee85c82da
websattard <85329551+websattard@users.noreply.github.com>
2021-06-28 00:35:13 +0300

    Переделка hero блока

--- scripts/parallaxOptions.js
+++
@@ -1,6 +0,0 @@
-var scene = document.getElementById('scene');
-var parallaxInstance = new Parallax(scene, {
-    pointerEvents: true,
-    hoverOnly: true
-});
-parallaxInstance.invert(false, false);
