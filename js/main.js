49ea0cd44cc3a65dfc20c3548eb98c8b495420e8
Sattard <sattard@rambler.ru>
2021-07-04 15:16:15 +0300

    изменение табуляции и структура футера закончена

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-
-var startX = [-109, -255, 108,-132, 0];
+var startX = [-109, -255, 108, -132, 0];
 var startY = [130, -336, 203, -111, 0];
 
 var deltaX = [0.04, 0.06, 0.08, 0, 0];
 var deltaY = [0.04, 0.06, 0.08, 0, 0];
-var deltaS = [ -1.8, -1.8, -1.8, 0, 0];
+var deltaS = [-1.8, -1.8, -1.8, 0, 0];
 
 var posX = 0; //переменные для хранения координаты X мыши
 var posY = 0; //переменные для хранения координаты Y мыши
@@ -13,31 +12,31 @@ var posS = 0; //переменные для хранения координат�
 var scene = $('.planet');
 
 function parallax() {
-    scene.css('background-position-x', 
-        startX.map(function (x, i) {
-            return x + posX * deltaX[i];
-    }).join('px, ') + 'px');
-    
-    scene.css('background-position-y', 
-            startY.map(function (y, i) {
-                return y + posY * deltaY[i];
-            }).join('px, ') + 'px');
+	scene.css('background-position-x',
+		startX.map(function (x, i) {
+			return x + posX * deltaX[i];
+		}).join('px, ') + 'px');
 
-    scene.css('background-position-y', 
-        startY.map(function (y, i) {
-            return y + posY * deltaY[i] + posS * deltaS[i];
-            }).join('px, ') + 'px');
-    };
+	scene.css('background-position-y',
+		startY.map(function (y, i) {
+			return y + posY * deltaY[i];
+		}).join('px, ') + 'px');
+
+	scene.css('background-position-y',
+		startY.map(function (y, i) {
+			return y + posY * deltaY[i] + posS * deltaS[i];
+		}).join('px, ') + 'px');
+};
 
 $(window).scroll(function () {
-    posS = $(document).scrollTop();
-    parallax();
+	posS = $(document).scrollTop();
+	parallax();
 });
 
 $(window).mousemove(function (evt) {
-    posX = evt.clientX;
-    posY = evt.clientY; /// window.innerHeight;
-    parallax();
+	posX = evt.clientX;
+	posY = evt.clientY; /// window.innerHeight;
+	parallax();
 });
 
 
@@ -48,4 +47,3 @@ $(window).mousemove(function (evt) {
 $(window).mousemove(function (evt) {
     parallax(-evt.pageX, evt.pageY / window.innerHeight);
 });*/
-
